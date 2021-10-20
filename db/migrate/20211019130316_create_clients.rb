7ef62b00ee8057cac0dd4625c74c22b1805685a2
mokkiko <maksim_pro@mail.ru>
2021-10-20 11:07:09 +0300

    add new migration

--- db/migrate/20211019130316_create_clients.rb
+++ db/migrate/20211019130316_create_clients.rb
@@ -1,14 +1,16 @@
+
 class CreateClients < ActiveRecord::Migration
   def change
-      create_table :clients do |t|
-            t.text :name
-            t.text :phone
-            t.text :datestamp
-            t.text :barber
-            t.text :color
 
+  	create_table :clients do |t|
+  		t.text :name
+  		t.text :phone
+  		t.text :datestamp
+  		t.text :barber
+  		t.text :color
 
-            t.timestamps
-            end
+  		t.timestamps
+  	end
+
   end
 end
